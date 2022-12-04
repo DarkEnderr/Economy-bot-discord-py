d8574c01ea93835736747d172b675233ab22430d
Nas(Sang) <zzenderzz07@gmail.com>
2022-12-04 22:32:41 +0700

    upload

---
+++ README.md
@@ -0,0 +1,77 @@
+# EcoBot
+A Economy Bot made using py-cord and mongoDB
+
+-------------------------
+
+## Usage
+
+Install the required modules
+
+```
+pip install -r requirements.txt
+```
+
+Create a file `data.json` with these contents
+
+```
+{
+    "name" : "Bot Name",
+    "token" : "Bot Token",
+    "mongo" : "Mongo Url (mongodb+srv://.......)"
+}
+```
+
+Edit `market.json` for items/products
+
+```
+{
+    "IoT" : [
+        ["Smart Watch",100,"smart-watch"]
+    ],
+    "Food" : [
+        ["Water",5,"water"]
+    ],
+    "Cars" : [
+        ["Car",10000,"car"]
+    ]
+}
+```
+
+For MongoDB create
+```
+Database -> eco
+            Collection -> money, bag
+```
+
+-------------------------
+
+#### Setup of mongodb
+
+- Go to https://www.mongodb.com
+
+- From there register/Try Free ( Fill up the details or register via google )
+
+- Verify Email
+
+- [Here for img](https://i.imgur.com/yPXOrcR.png)
+
+Click on project 0 , then click on new project
+
+- Name your project ( anything ), then add members ( if any ). After that click create.
+
+- [Here for img](https://i.imgur.com/BeA2t9P.png)
+
+Click on build database, select Free (for starters), let the default settings be there, but you can chnage the last field of cluster name.
+
+- Then let the default things be selected and enter your username/password ( remember them ).
+
+- In the IP access list enter `0.0.0.0/0` (allow from everywhere).
+
+- Finish and clear
+
+- After that it will take some time to create. Once its finished go to Connect and select Connect your application. Select python and 3.6 or later. Then copy the link and paste in data.json. Edit the password (remove < > too). And done
+
+- Now go to Browse Collections and click `Add My Own Data` . Enter whatever db and collection name you want to use.
+Later to connect to that db you can do `foo = cluster["database"]["collection"]` in the code. Like `ecomoney = cluster["eco"]["money"]`.
+
+- That's it for db setup.
---
+++ main.py
@@ -0,0 +1,87 @@
+from datetime import datetime, timedelta
+from os import listdir, system
+
+import aiohttp
+import discord
+import json
+
+from discord.ext import commands
+from pretty_help import PrettyHelp
+
+
+class Echo(commands.Bot):
+    def __init__(self):
+        self.description = """Echo - A Economy Bot"""
+
+        super().__init__(
+            command_prefix={"."},
+            owner_ids={727365670395838626},
+            intents=discord.Intents.all(),
+            help_command=PrettyHelp(),
+            description=self.description,
+            case_insensitive=True,
+            start_time=datetime.utcnow(),
+        )
+
+    async def on_connnect(self):
+        self.session = aiohttp.ClientSession(loop=self.loop)
+
+        cT = datetime.now() + timedelta(
+            hours=5, minutes=30
+        )  # GMT+05:30 is Our TimeZone So.
+
+        print(
+            f"[ Log ] {self.user} Connected at {cT.hour}:{cT.minute}:{cT.second} / {cT.day}-{cT.month}-{cT.year}"
+        )
+
+    async def on_ready(self):
+        cT = datetime.now() + timedelta(
+            hours=5, minutes=30
+        )  # GMT+05:30 is Our TimeZone So.
+
+        print(
+            f"[ Log ] {self.user} Ready at {cT.hour}:{cT.minute}:{cT.second} / {cT.day}-{cT.month}-{cT.year}"
+        )
+        print(f"[ Log ] GateWay WebSocket Latency: {self.latency*1000:.1f} ms")
+
+with open('./data.json') as f:
+  d1 = json.load(f)
+with open('./market.json') as f:
+  d2 = json.load(f)
+
+def bot_info():
+    return d1
+def market_info():
+    return d2
+
+TOKEN = d1['token']
+bot = Echo()
+
+@bot.command(hidden=True)
+@commands.is_owner()
+async def load(ctx, extension):
+    bot.load_extension(f"cogs.{extension}")
+    await ctx.send("Done")
+
+
+@bot.command(hidden=True)
+@commands.is_owner()
+async def unload(ctx, extension):
+    bot.unload_extension(f"cogs.{extension}")
+    await ctx.send("Done")
+
+
+@bot.command(hidden=True)
+@commands.is_owner()
+async def reload(ctx, extension):
+    bot.unload_extension(f"cogs.{extension}")
+    bot.load_extension(f"cogs.{extension}")
+    await ctx.send("Done")
+
+
+for filename in listdir("./cogs"):
+    if filename.endswith(".py"):
+        bot.load_extension(f"cogs.{filename[:-3]}")
+
+bot.load_extension("jishaku")
+bot.loop.run_until_complete(bot.run(TOKEN))
